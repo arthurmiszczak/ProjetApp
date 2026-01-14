35ec59664d3aa83d243d69bde161e8496f41c17a
arthurmiszczak <Arthur.arthur.miszczak@gmail.com>
2026-01-14 11:34:28 +0100

    add html sur le server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Express');
+app.get('/login', (req, res) => {
+    res.send('Binvenue sur la page de login');
 });
+
+
+
+
+
+
+app.post('/register', (req, res) => {
+    res.send('Merci de vous crée un compte');
+});
+
+
+
+
+
+app.use(express.static('public'));
+
+
+
 
 app.listen(3000, () => {
     let monIp = require('ip').address();
-    consol.log('Server running on http://$(monIp):3000');
+    console.log(`Server running on http://${monIp}:3000`);
 });
